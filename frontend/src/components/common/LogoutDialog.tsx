d4f57fa0d62c9cc21dd4dbf9f1fad8e1a445e26f
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-06-17 00:09:23 -0700

    Added Logout Dialog component

---
+++ frontend/src/components/common/LogoutDialog.tsx
@@ -0,0 +1,35 @@
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export type LogoutDialogProps = {
+    open: boolean,
+    onClose: () => void,
+}
+export function LogoutDialog({open, onClose} : LogoutDialogProps) {
+    return (
+        <>
+            <Dialog
+                open={open}
+                onClose={onClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                role="alertdialog"
+            >
+                <DialogTitle id="alert-dialog-title" sx={{ color: "black" }}>
+                    {"Are you sure you want to log out?"}
+                </DialogTitle>
+                <DialogContent>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onClose} autoFocus>
+                        Cancel
+                    </Button>
+                    <Button onClick={onClose}>Yes, log out</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
