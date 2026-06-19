320c716bf077b4e5e39d4738aebfdd6a0545d9bd
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-06-19 02:13:02 -0700

    Implemented removing of bookings

--- frontend/src/components/residents/laundryBookings/removeBookings/CancelBookingButton.tsx
+++ frontend/src/components/residents/laundryBookings/removeBookings/CancelBookingButton.tsx
@@ -2,20 +2,29 @@ import {useState} from "react";
 import SelectBookings from "./SelectBookings.tsx";
 import {Button} from "@mui/material";
 import {ResponsiveDialog} from "../../../common/ResponsiveDialog.tsx";
+import {useDispatch} from "react-redux";
+import {removeBooking} from "../../../../context/residents/bookingsSlice.ts";
 
 export function CancelBookingButton() {
     const [open, setOpen] = useState(false);
+    const dispatch = useDispatch();
+    const [bookingId, setBookingId] = useState("");
+
     const handleClickOpen = () => {
         setOpen(true);
     };
     const handleClose = () => {
         setOpen(false);
     };
+    const handleSubmit = () => {
+        dispatch(removeBooking(bookingId));
+        setOpen(false);
+    }
 
     const content = <div><strong>Cancel Booking?</strong></div>
     const actions = <div>
-        <SelectBookings/>
-        <Button onClick={handleClose}>Confirm</Button>
+        <SelectBookings value={bookingId} setValue={setBookingId}/>
+        <Button onClick={handleSubmit}>Confirm</Button>
         <Button onClick={handleClose}>Cancel</Button>
     </div>
 
