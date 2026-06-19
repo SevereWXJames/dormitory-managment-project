f6e11c479bf35f55e1ae88eaa1a7a753b3888b0d
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-06-19 09:53:45 -0700

    Hardcoded dashboard

--- frontend/src/components/residents/laundryBookings/bookingHistory/UserBookings.tsx
+++ frontend/src/components/residents/laundryBookings/bookingHistory/UserBookings.tsx
@@ -5,10 +5,6 @@ import type {Booking} from "../../../../types/residents/types.tsx";
 import {getBookingsByUser} from "../../../../context/residents/bookingsSlice.ts";
 
 export function RecentBookings() {
-    // const rows: bookingData[] = [{
-    //     event_title: 'Washing hoodie',
-    //     start_time: '11:00', end_time: '12:00', date: '06/17/26', machine_num: 1
-    // }];
     const user = useSelector(getUsername);
     const rows: Booking[] = useSelector(getBookingsByUser(user))
     return (
