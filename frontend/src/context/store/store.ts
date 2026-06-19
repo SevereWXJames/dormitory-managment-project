c672ffe3100ddfde78ebeae27b58f48da6fc24bb
Ronaldo Kanegae <ronaldo.kanegae@gmail.com>
2026-06-19 09:59:39 -0700

    Fixed missing Maintenance Request Reducer

--- frontend/src/context/store/store.ts
+++ frontend/src/context/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from '../authenticationSlice.ts';
 import creditsReducer from '../residents/creditsSlice.ts';
 import bookingsReducer from '../residents/bookingsSlice.ts';
+import MaintenanceRequestReducer from '../residents/maintenanceRequestsSlice.ts';
 
 export const store = configureStore({
 	reducer: {
 		authentication: authenticationReducer,
 		credits: creditsReducer,
-        bookings: bookingsReducer
+        bookings: bookingsReducer,
+		maintenanceRequests: MaintenanceRequestReducer
 	},
 });
 
