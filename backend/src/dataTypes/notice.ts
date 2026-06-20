94af5350e2b77dd238454722a6689effc625bb11
SevereWXJames <severewxjames@gmail.com>
2026-06-19 18:49:16 -0700

    Update notice data type on backend.

--- backend/src/dataTypes/notice.ts
+++ backend/src/dataTypes/notice.ts
@@ -4,12 +4,14 @@ export class Notice {
     public viewableBy: [string] | null;
     public title: string;
     public text: string;
+    public createAt: number;
 
-    constructor({_id, createdBy, viewableBy, title, text}: {_id: string, createdBy: string, viewableBy: [string] | null, title: string, text: string}) {
+    constructor({_id, createdBy, viewableBy, title, text, createdAt}: {_id: string, createdBy: string, viewableBy: [string] | null, title: string, text: string, createdAt: number}) {
         this._id = _id;
         this.createdBy = createdBy;
         this.viewableBy = viewableBy;
         this.title = title;
         this.text = text;
+        this.createAt = createdAt;
     }
 }
