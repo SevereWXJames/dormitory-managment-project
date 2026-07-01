502118d26c4e75b6b89814611202eb721f15015a
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-06-30 17:49:59 -0700

    Updated HomePage.tsx to only render the LoginPage

--- frontend/src/pages/common/HomePage.tsx
+++ frontend/src/pages/common/HomePage.tsx
@@ -1,19 +1,34 @@
-import { CommonFrame } from "../../components/common/CommonFrame";
+// import { CommonFrame } from "../../components/common/CommonFrame";
+import {LoginPage} from "@/pages/common/LoginPage.tsx";
 
 /**
  * Home page React component, for unauthenticated users.
- * 
+ *
  * @returns JSX for the home page.
  */
 export function HomePage() {
-	return (
-		<>
-			<CommonFrame commonFrameType="UNAUTHENTICATED">
-			<main>
-				<h1>SmartAPT</h1>
-				<p>Log in to access your building's portal.</p>
-			</main>
-            </CommonFrame>
-		</>
-	);
+    return (
+        <>
+            <LoginPage/>
+        </>
+    );
 }
+
+//
+// /**
+//  * Home page React component, for unauthenticated users.
+//  *
+//  * @returns JSX for the home page.
+//  */
+// export function HomePage() {
+// 	return (
+// 		<>
+// 			<CommonFrame commonFrameType="UNAUTHENTICATED">
+// 			<main>
+// 				<h1>SmartAPT</h1>
+// 				<p>Log in to access your building's portal.</p>
+// 			</main>
+//             </CommonFrame>
+// 		</>
+// 	);
+// }
