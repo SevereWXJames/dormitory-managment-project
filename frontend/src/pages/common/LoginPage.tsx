70c071ab0dc06b070239e69ef422ca23f6e3cb68
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-06-30 17:49:59 -0700

    Updated HomePage.tsx to only render the LoginPage

--- frontend/src/pages/common/LoginPage.tsx
+++ frontend/src/pages/common/LoginPage.tsx
@@ -10,14 +10,16 @@ export function LoginPage() {
     return (
         <>
             <CommonFrame commonFrameType="UNAUTHENTICATED">
-            <main className="login-page">
-                <Card className="flex flex-col gap-4 p-4 m-4 w-fit mx-auto">
-                    <CardTitle className="text-2xl">Log in</CardTitle>
-                    <CardContent className="flex flex-col gap-4">
-                        <LoginForm/>
-                    </CardContent>
-                </Card>
-            </main>
+                <h1>SmartAPT</h1>
+                <p>Log in to access your building's portal.</p>
+                <main className="login-page">
+                    <Card className="flex flex-col gap-4 p-4 m-4 w-fit mx-auto">
+                        <CardTitle className="text-2xl">Log in</CardTitle>
+                        <CardContent className="flex flex-col gap-4">
+                            <LoginForm/>
+                        </CardContent>
+                    </Card>
+                </main>
             </CommonFrame>
         </>
     )
