96995ba53748046ba910d5969cd69fca0c32222b
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-01 21:36:56 -0700

    Refactored CreditsPage

---
+++ frontend/src/components/residents/credits/hooks/useCreditsBalanceCard.tsx
@@ -0,0 +1,10 @@
+// components/residents/credits/CreditsBalanceCard.tsx
+// import { useSelector } from "react-redux";
+// import { getUserId } from "@/context/authenticationSlice.ts";
+import {useCreditsData} from "@/pages/common/residents/pageHooks/useCreditsData.tsx";
+
+export function useCreditsBalanceCard() {
+    const {loading, error, balanceCents} = useCreditsData();
+    return {balance: balanceCents, error, loading}
+
+}
