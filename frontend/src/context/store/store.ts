6bf7adaa0cdaa82cef162411729902c30b727d33
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-01 20:27:35 -0700

    Refactored api calls

--- frontend/src/context/store/store.ts
+++ frontend/src/context/store/store.ts
@@ -3,15 +3,42 @@ import authenticationReducer from '../authenticationSlice.ts';
 import creditsReducer from '../residents/creditsSlice.ts';
 import bookingsReducer from '../residents/bookingsSlice.ts';
 import MaintenanceRequestReducer from '../residents/maintenanceRequestsSlice.ts';
+import {api} from "@/context/api/api.ts";
 
 export const store = configureStore({
-	reducer: {
-		authentication: authenticationReducer,
-		credits: creditsReducer,
+    reducer: {
+        authentication: authenticationReducer,
+        credits: creditsReducer,
         bookings: bookingsReducer,
-		maintenanceRequests: MaintenanceRequestReducer
-	},
+        maintenanceRequests: MaintenanceRequestReducer,
+        [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
+
+
+
+
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import authenticationReducer from '../authenticationSlice.ts';
+// import creditsReducer from '../residents/creditsSlice.ts';
+// import bookingsReducer from '../residents/bookingsSlice.ts';
+// import MaintenanceRequestReducer from '../residents/maintenanceRequestsSlice.ts';
+//
+// export const store = configureStore({
+// 	reducer: {
+// 		authentication: authenticationReducer,
+// 		credits: creditsReducer,
+//         bookings: bookingsReducer,
+// 		maintenanceRequests: MaintenanceRequestReducer
+// 	},
+// });
+//
+// export type RootState = ReturnType<typeof store.getState>;
+//
--- frontend/src/pages/common/HomePage.tsx
+++ frontend/src/pages/common/HomePage.tsx
@@ -13,22 +13,3 @@ export function HomePage() {
         </>
     );
 }
-
-//
-// /**
-//  * Home page React component, for unauthenticated users.
-//  *
-//  * @returns JSX for the home page.
-//  */
-// export function HomePage() {
-// 	return (
-// 		<>
-// 			<CommonFrame commonFrameType="UNAUTHENTICATED">
-// 			<main>
-// 				<h1>SmartAPT</h1>
-// 				<p>Log in to access your building's portal.</p>
-// 			</main>
-//             </CommonFrame>
-// 		</>
-// 	);
-// }
