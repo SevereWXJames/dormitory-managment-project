4030d44ecdc46379686560bfd998030db24c83cf
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-01 23:25:15 -0700

    Refactored NoticesPage

---
+++ frontend/src/pages/common/residents/pageHooks/useNoticesData.tsx
@@ -0,0 +1,17 @@
+import {useSelector} from "react-redux";
+import {getUserId} from "@/context/authenticationSlice.ts";
+import {useGetNoticesByUserQuery} from "@/context/api/services/noticesApi.ts";
+
+export function useNoticesData() {
+    const userId = useSelector(getUserId);
+    const {data, isLoading, isError, error} = useGetNoticesByUserQuery(userId!, {skip: !userId});
+
+    return {
+        notices: data,
+        isLoading,
+        isError,
+        error: !userId
+            ? "User is not authenticated."
+            : isError ? error?.message : null,
+    };
+}
