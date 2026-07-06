09ce36a44781ce0197582be30f2c61a5a4e617eb
SevereWXJames <severewxjames@gmail.com>
2026-07-06 02:04:46 -0700

    Basic version of IoT Functionality

--- backend/src/dataTypes/service.ts
+++ backend/src/dataTypes/service.ts
@@ -1,22 +1,19 @@
 import mongoose, {Schema} from "mongoose";
 import type {CollectionName} from "../database/databaseConstants.ts";
 
-export type MongoId = string | mongoose.Types.ObjectId;
-
 export interface Service {
-    _id: MongoId;
+    _id: string;
     name: string;
     description: string;
     hasIoT: boolean;
-    IoTName: string | null;
+    IoTUUID: string | null;
+    IoTType: string | null;
     reservationDurationSeconds: number;
     reservationStartHour: number;
     reservationEndHour: number;
 }
 
-export type ServiceInput = Omit<Service, "_id"> & { _id?: MongoId };
-
-const serviceSchema = new Schema({name: String, description: String, hasIoT: Boolean,
-    IoTName: String, reservationDurationSeconds: Number,
+const serviceSchema = new Schema({_id: String, name: String, description: String, hasIoT: Boolean,
+    IoTUUID: String, IoTType: String, reservationDurationSeconds: Number,
     reservationStartHour: Number, reservationEndHour: Number});
 export const ServiceModel = mongoose.model("Services"  as CollectionName, serviceSchema, "Services"  as CollectionName);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,18 +12,20 @@ import servicesRouter from "./routes/services.ts";
 import noticeRouter from "./routes/notices.ts";
 import reservationRouter from "./routes/reservation.ts";
 import residentsRouter from "./routes/residents.ts";
-import {setUpMQTT} from "./utility/mqttSetup.ts";
 import loadSampleData from "./database/loadDatabase.ts";
+import {connectMongo} from "./database/database.ts";
+import {setUpMQTT} from "./utility/mqttSetup.ts";
 
 dotenv.config();
 
+await connectMongo();
 setUpMQTT();
 
 const app = express();
 const port: number = 3000;
 
 if (process.env.LOAD_SAMPLE_DATA !== undefined) {
-   loadSampleData();
+   await loadSampleData();
 }
 
 app.use(json());
