8ae541e0fe760263fdbc2211bb928c8a522bcad5
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-06 16:56:23 -0700

    Fixed some failing tests in signup

--- backend/src/database/tableOperations/User.table.ts
+++ backend/src/database/tableOperations/User.table.ts
@@ -2,33 +2,34 @@ import {type IUser, Users} from "../models/users.model.ts";
 import {BaseTable} from "./Base.table.ts";
 import type {SignUpRequest} from "../types/user.service.types.ts";
 
-export class UserTable extends BaseTable<IUser>{
+export class UserTable extends BaseTable<IUser> {
     constructor() {
         super(Users);
     }
 
-    async createUser(profileData : SignUpRequest){
-        try{
-            console.log(`Creating user: ${JSON.stringify(profileData)}`);
+    // Creates a user in the Users table.
+    // If the user is a resident, creates a resident in the Resident table and a balance in the CreditBalance table
+    async createUser(profileData: SignUpRequest) {
+        try {
             const doc = new Users(profileData);
             await doc.save();
-        }catch(error){
+        } catch (error) {
             console.log(`Error:${error}`);
             throw Error("Error creating user", {cause: error});
         }
 
     }
 
-    async findNewlyCreatedUser(profileData : SignUpRequest){
-        return await Users.find(profileData).exec();
+    async findNewlyCreatedUser(profileData: SignUpRequest) {
+        return await Users.findOne(profileData).exec();
     }
 
-    async deleteUser(username: string, email:string){
+    async deleteUser(username: string, email: string) {
         const filter = {username: username, email: email}
         return await Users.findOneAndDelete(filter).exec();
     }
 
-    async findUser(username: string, email:string){
+    async findUser(username: string, email: string) {
         const filter = {username: username, email: email}
         return await Users.find(filter).exec();
     }
