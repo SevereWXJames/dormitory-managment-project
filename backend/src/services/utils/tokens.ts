d81f2a97658e6a55be044765899549b045ba7eed
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-06 23:46:07 -0700

    Implemented test for logout

--- backend/src/services/utils/tokens.ts
+++ backend/src/services/utils/tokens.ts
@@ -11,21 +11,3 @@ export const createJWTToken = (id: null | Types.ObjectId) => {
     if(!key) throw Error("Error creating jwt token");
     return jwt.sign({userId: id}, key, {expiresIn: "7d"}); //jwt token
 }
-
-// signing the access token
-export const createAccessToken = (id: string | Types.ObjectId) => {
-    const key = process.env.ACCESS_TOKEN_SECRET;
-    if(!key) throw Error("Error creating access token");
-    return sign({ id }, key, {
-        expiresIn: 15 * 60,
-    });
-};
-
-// signing the refresh token
-export const createRefreshToken = (id: string | Types.ObjectId) => {
-    const key = process.env.ACCESS_TOKEN_SECRET;
-    if(!key) throw Error("Error creating refresh token");
-    return sign({ id }, key, {
-        expiresIn: "90d",
-    });
-};
