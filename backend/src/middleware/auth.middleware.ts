1e9da62942cb855a60815679ea7a15b65dfc841b
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-07 16:34:23 -0700

    Implemented middleware and tests for role authorization

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import type {NextFunction,Response, Request} from "express";
-import {verifyRequestHeader} from "./services/middlerware.service.ts";
+import {verifyRequestHeader, verifyRoles} from "./services/middlerware.service.ts";
+import type {Role} from "../database/types/user.service.types.ts";
 
 //Auth Middleware
 export const authenticateRequest = async (req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +12,21 @@ export const authenticateRequest = async (req: Request, res: Response, next: Nex
     }catch(error){
         return res.status(500).json({
             message: "Authentication failed",
+            type: "error",
+            error: (error as Error).message,
+        });
+    }
+}
+
+export const authorizeRole = async (req: Request, res: Response, next: NextFunction, allowedRoles: Role[]) => {
+    // get the token from the header
+    try{
+        console.log(`req headers: ${JSON.stringify(req.headers)}`);
+        await verifyRoles(req, allowedRoles);
+        next();
+    }catch(error){
+        return res.status(500).json({
+            message: "Authorization failed.",
             type: "error",
             error: (error as Error).message,
         });
