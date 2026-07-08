ca67d6ef3abaa6a719683b3b31588de693d339ea
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-08 00:18:19 -0700

    Added clearDB method for testing purposes to database.ts

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -42,6 +42,7 @@ export async function connectMongo(quiet: boolean = false): Promise<void> {
         try {
             if (!quiet) console.info(`Database.create(): Attempting to connect to ${host}.`);
             await mongoose.connect(host, options);
+            //clearDB(); for testing purposes
             if (!quiet) console.info(`Database.create(): Successfully connected to ${host}.`);
             return;
         } catch (e) {
@@ -51,6 +52,14 @@ export async function connectMongo(quiet: boolean = false): Promise<void> {
     }
 
     return Promise.reject(new Error("Database.create(): Failed to connect to all specified hosts."));
+}
+
+async function clearDB(){
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        if(!collections[key]) continue;
+        await collections[key].deleteMany({});
+    }
 }
 
 /**
