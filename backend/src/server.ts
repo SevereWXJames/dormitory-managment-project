434a10549f97d0964812355c488d2d2789a0bf3b
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-08 00:06:22 -0700

    Fixed connection issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,6 @@
-import dotenv from "dotenv";
 import {connectMongo} from "./database/database.ts";
 import app from "./app.ts";
 
-dotenv.config();
 const port: number = 3000;
 
 await connectMongo().then(() => {
