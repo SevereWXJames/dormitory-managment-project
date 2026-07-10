115ee25aed608f5a4cdee54d3f2534fdfbbd7b63
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-09 20:27:03 -0700

    Add IoTDataServices to IoT folder

--- backend/src/dataTypes/service.ts
+++ backend/src/dataTypes/service.ts
@@ -11,7 +11,7 @@ export interface Service {
     reservationDurationSeconds: number;
     reservationStartHour: number;
     reservationEndHour: number;
-    IoTName: string;
+    // IoTName: string;
 }
 
 const serviceSchema = new Schema({_id: String, name: String, description: String, hasIoT: Boolean,
