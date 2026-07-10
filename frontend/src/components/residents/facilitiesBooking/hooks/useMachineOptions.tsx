08e70306645514c2621c4b030a866afd78307fff
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-10 16:15:03 -0700

    Added method for converting from unix time to human readable date

--- frontend/src/components/residents/facilitiesBooking/hooks/useMachineOptions.tsx
+++ frontend/src/components/residents/facilitiesBooking/hooks/useMachineOptions.tsx
@@ -2,9 +2,10 @@ import {useGetServicesQuery} from "@/context/api/apiServices/servicesApi.ts";
 
 export function useMachineOptions(){
     const {data: machineServices, isLoading, isError, error} = useGetServicesQuery();
-    console.log(`data: ${machineServices}`);
+
     const machines = machineServices?.map((service) =>
         ({id: service._id, name: service.name}));
+
     return {isLoading,
         isError : isError,
         error,
