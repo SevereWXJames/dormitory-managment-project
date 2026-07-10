be7d7af13f8fcc6e8a45a4f76b3ca573d9dad870
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-10 00:52:53 -0700

    Implemented routes for new methods in reservationRouter.ts

--- backend/src/routes/reservation.ts
+++ backend/src/routes/reservation.ts
@@ -1,5 +1,10 @@
 import express, {type Request, type Response, type NextFunction} from "express";
-import {getReservationsBookedByUserId, getReservationsSlotsByServiceId} from "../services/reservationServices.ts";
+import {
+    bookReservationSlot,
+    getAllFreeSlots, getAllReservedSlots,
+    getReservationsBookedByUserId,
+    getReservationsSlotsByServiceId
+} from "../services/reservationServices.ts";
 
 const reservationRouter = express.Router();
 
@@ -32,6 +37,55 @@ reservationRouter.get("/get-slots-by-service/:serviceId", async (req: Request, r
     }
     catch (error) {
         return res.status(500).json({success: false, message: "Internal server error."});
+    }
+});
+
+reservationRouter.get("/get-free-slots-by-service/:serviceId", async (req: Request, res: Response)=> {
+    if (req.params.serviceId === undefined) {
+        return res.status(400).json({success: false, message: "No ID provided."});
+    }
+
+    try {
+        const reservationSlots = await getAllFreeSlots(req.params.serviceId as string);
+        return res.status(200).json({success: true, data: reservationSlots});
+    }
+    catch (error) {
+        return res.status(500).json({success: false, message: "Internal server error.", error: error});
+    }
+});
+
+reservationRouter.get("/get-all-reserved-slots-by-service/:serviceId", async (req: Request, res: Response)=> {
+    if (req.params.serviceId === undefined) {
+        return res.status(400).json({success: false, message: "No ID provided."});
+    }
+
+    try {
+        const reservationSlots = await getAllReservedSlots(req.params.serviceId as string);
+        return res.status(200).json({success: true, data: reservationSlots});
+    }
+    catch (error) {
+        return res.status(500).json({success: false, message: "Internal server error.", error: error});
+    }
+});
+
+reservationRouter.put("/book-slot-by-service/:serviceId", async (req: Request, res: Response)=> {
+    const {slotId} = req.body;
+    const {id} = req.user;
+    const {serviceId} = req.params;
+
+    if (!serviceId) {
+        return res.status(400).json({success: false, message: "No ID provided."});
+    }
+    if(!slotId){
+        return res.status(400).json({success: false, message: "No slot provided."});
+    }
+
+    try {
+        const reservationSlots = await bookReservationSlot(serviceId, slotId, id);
+        return res.status(200).json({success: true, data: reservationSlots});
+    }
+    catch (error) {
+        return res.status(500).json({success: false, message: "Internal server error.", error: error});
     }
 });
 
