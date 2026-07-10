1beb1b4bc5e515b2baf5fae0f276d93878a91d96
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-10 14:26:38 -0700

    Added new api query, getSlotsByService

--- frontend/src/context/api/apiServices/reservationSlotsApi.ts
+++ frontend/src/context/api/apiServices/reservationSlotsApi.ts
@@ -1,7 +1,6 @@
 import { api } from "../api";
 import type {ReservationSlot} from "@/dataTypes/reservationSlot.ts";
 import type {Booking} from "@/types/residents/types.tsx";
-
 //Helper to convert response to Booking
 function toBooking(slot: ReservationSlot): Booking {
     const startDate = new Date(slot.startTime * 1000);
@@ -24,6 +23,13 @@ export const reservationSlotsApi = api.injectEndpoints({
             transformResponse: (reservations: ReservationSlot[]) => reservations.map(toBooking),
             providesTags: ["ReservationSlots"],
         }),
+
+        getSlotsByService: builder.query<ReservationSlot[], string>({
+            query: (serviceId) => ({ url: `/get-slots-by-service/${encodeURIComponent(serviceId)}`}),
+            providesTags: ["ReservationSlots"],
+        }),
+
+
     }),
 });
 
