6c79e4e3461a73bcc6e0e83fecd71406bd50f1f3
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-10 16:45:02 -0700

    Fixed timeslot date formatting

--- frontend/src/components/residents/facilitiesBooking/TimeSlot.tsx
+++ frontend/src/components/residents/facilitiesBooking/TimeSlot.tsx
@@ -6,6 +6,6 @@ type TimeSlotProps = {
 }
 export function TimeSlot(props: TimeSlotProps){
     return(<>
-        <Button className="p-4 m-2 width:fit-content" variant="outline">{`Start: ${props.slot.startTimeString} | Duration: 1 hour`}</Button>
+        <Button className="p-4 m-2 width:fit-content" variant="outline" disabled={!props.slot.booked}>{`Start: ${props.slot.startTimeString} | Duration: 1 hour`}</Button>
     </>)
 }
