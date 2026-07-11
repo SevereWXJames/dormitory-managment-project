4fee84f77be841fa0729608e16e6b477b15d156f
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-10 20:54:19 -0700

    Fixed error where cannot log back in as an admin

--- frontend/src/components/common/Auth/hooks/useLoginForm.tsx
+++ frontend/src/components/common/Auth/hooks/useLoginForm.tsx
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useLoginMutation} from "@/context/api/apiServices/authApi.ts";
+import {Role} from "@/dataTypes/user.ts";
 
 export function useLoginForm() {
     const [username, setUsername] = useState("");
@@ -39,8 +40,9 @@ export function useLoginForm() {
                 password,
             }).unwrap();
 
+            console.log(`data roles: ${data.roles}`);
             const targetPath = (data.roles ?? []).some((role) =>
-                role === "Admin" || role === "Staff") ?
+                role === Role.ADMIN || role == "Staff") ?
                 "/admin/dashboard" : "/dashboard";
             navigate(targetPath);
         } catch (error) {
