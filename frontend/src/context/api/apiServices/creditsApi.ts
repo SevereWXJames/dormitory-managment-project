7fb5afeb4e70d05c1a1b7d1ed911f4825e1a78a2
Ronaldo Kanegae <ronaldo.kanegae@gmail.com>
2026-07-10 20:27:41 -0700

    Connection of credits page to backend, including adding credits and getting credits and transaction history

--- frontend/src/context/api/apiServices/creditsApi.ts
+++ frontend/src/context/api/apiServices/creditsApi.ts
@@ -2,6 +2,8 @@
 import { api } from "../api";
 import type { CreditBalance, Transaction } from "@/dataTypes/creditBalance.ts";
 
+export type AddCreditsRequestType = {userId: string, creditsCents: Number};
+
 export const creditsApi = api.injectEndpoints({
     endpoints: (builder) => ({
         getCreditBalance: builder.query<CreditBalance, string>({
@@ -12,7 +14,14 @@ export const creditsApi = api.injectEndpoints({
             query: (userId) => ({ url: `/credits/get-transaction-history/${encodeURIComponent(userId)}` }),
             providesTags: ["Transactions"],
         }),
+        putAddCredits: builder.mutation<Number, AddCreditsRequestType>({
+            query: ({userId: userId, creditsCents: creditsCents}) => ({
+                url: `/credits/add-credits/${encodeURIComponent(userId)}`,
+                method: "PUT",
+                body: {amount: creditsCents}
+            })
+        })
     }),
 });
 
-export const { useGetCreditBalanceQuery, useGetTransactionHistoryQuery } = creditsApi;
+export const { useGetCreditBalanceQuery, useGetTransactionHistoryQuery, usePutAddCreditsMutation } = creditsApi;
