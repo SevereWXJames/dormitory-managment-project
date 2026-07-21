a6eb625de21153fd3a497544dbdf6c08fa536c0a
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-20 23:22:19 -0700

    Added method createRefreshToken

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,9 @@
-import express, {type Request, type Response} from "express";
+import express, {type CookieOptions, type Request, type Response} from "express";
 import {
     logIn,
     signUp
 } from "../services/usersServices.ts";
-import {createJWTToken} from "../utility/auth-utils/tokens.ts";
+import {createAccessToken} from "../utility/auth-utils/tokens.ts";
 
 const authRouter = express.Router();
 authRouter.post("/signup", async (req, res) => {
@@ -11,14 +11,15 @@ authRouter.post("/signup", async (req, res) => {
         const { name, username, email, password, phoneNumber, roles } = req.body;
         const profileData = {name, username, email, password, phoneNumber, roles };
         const user = await signUp(profileData);
-        const token = createJWTToken(user._id, user.roles);
-        res.cookie("jwt", token, {
+        const token = createAccessToken(user._id, user.roles);
+        const cookiePayload : CookieOptions = {
             httpOnly: true,       // JS cannot read this cookie — protects against XSS
             secure: true,          // only sent over HTTPS (set false only for local http dev)
             sameSite: "strict",    // or "lax" — see note below on cross-site setups
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days, matches JWT expiry
             path: "/",
-        });
+        };
+        res.cookie("jwt", token, cookiePayload);
 
         res.status(200).json({
             message: "Signed up successfully!",
@@ -43,7 +44,7 @@ authRouter.post("/login", async (req: Request, res: Response)=> {
     let {username, email, password} = req.body;
     try {
         const user = await logIn(username, email, password);
-        const token = createJWTToken(user._id, user.roles);
+        const token = createAccessToken(user._id, user.roles);
         res.cookie("jwt", token, {
             httpOnly: true,       // JS cannot read this cookie — protects against XSS
             secure: true,          // only sent over HTTPS (set false only for local http dev)
