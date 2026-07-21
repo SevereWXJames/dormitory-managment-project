38c4057b7398418d29ea8a6344aeba5cfbc19f75
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-21 00:54:35 -0700

    Updated tests

--- backend/test/auth/authorization.test.ts
+++ backend/test/auth/authorization.test.ts
@@ -68,7 +68,7 @@ describe('AUTHORIZATION', () => {
                     .send(adminLoginPayload);
                 const userId = res.body.data._id;
                 const cookies = res.headers['set-cookie'] as unknown as string[];
-                const rawCookie = cookies.find((c) => c.startsWith('jwt='));
+                const rawCookie = cookies.find((c) => c.startsWith('access='));
 
                 testJwt = rawCookie?.split(';')[0];
                 testUserId = userId;
@@ -125,7 +125,7 @@ describe('AUTHORIZATION', () => {
                     .send(residentLoginPayload);
                 const userId = res.body.data._id;
                 const cookies = res.headers['set-cookie'] as unknown as string[];
-                const rawCookie = cookies.find((c) => c.startsWith('jwt='));
+                const rawCookie = cookies.find((c) => c.startsWith('access='));
 
                 testJwt = rawCookie?.split(';')[0];
                 testUserId = userId;
