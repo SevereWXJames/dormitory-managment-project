b0968652e72f50119e3250162e63bdf42d9c7f8d
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-22 19:03:37 -0700

    Updated response format for refresh

--- backend/src/services/usersServices.ts
+++ backend/src/services/usersServices.ts
@@ -54,7 +54,7 @@ export async function getExistingUserFromId(_id: mongoose.Types.ObjectId): Promi
 }
 
 export async function getUserByQuery(filter : Object){
-    const doc = await UserModel.findOne(filter).exec();
+    const doc = await UserModel.findOne(filter).lean().exec();
     if(!doc) throw Error("Error, user not found");
     return { ...doc, _id: doc._id.toString() } as unknown as User;
 }
