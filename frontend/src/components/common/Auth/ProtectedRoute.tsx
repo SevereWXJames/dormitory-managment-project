389ce925a776adecf2a7d990b87c8856dce144cc
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-07-22 17:59:26 -0700

    Edited ProtectedRoutes to use Navigate rather than returning the pages

--- frontend/src/components/common/Auth/ProtectedRoute.tsx
+++ frontend/src/components/common/Auth/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import type {Role} from "@/dataTypes/user.ts";
 import {useSelector} from "react-redux";
 import {getUserId, getUserRole} from "@/context/authenticationSlice.ts";
-import {UnauthorizedPage} from "@/pages/UnauthorizedPage.tsx";
+import {Navigate} from "react-router-dom";
 
 interface ProtectedRouteProps {
     allowedRoles: Role[];
@@ -16,8 +16,11 @@ export function ProtectedRoute({allowedRoles, children} : ProtectedRouteProps){
     console.log(`allowedRoles: ${allowedRoles}`);
 
     const hasPermission = userRoles.some(role => allowedRoles.includes(role));
-    if(!hasPermission || !userId){
-        return <UnauthorizedPage/>
+    if (!userId){
+        return <Navigate to="/login" replace />;
+    }
+    if(!hasPermission){
+        return <Navigate to="/unauthorized" replace />;
     }
 
     return <>{children}</>;
