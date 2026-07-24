ea1ceb7425ad98d5d9d64d852bc9c5521e773263
Zhuoyan Qiu(Leo) <zhyqiu@students.cs.ubc.ca>
2026-07-24 14:27:01 -0700

    conflict on server.ts solved

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,10 @@
 import {connectMongo} from "./database/database.ts";
 import app from "./app.ts";
-import {setUpMQTT} from "./utility/mqttSetup.ts";
-<<<<<<< HEAD
+import {setUpMQTT} from "./utility/mqttSetup.ts";g
 import loadSampleData, { ensureAdminExists } from "./database/loadDatabase.ts";
-=======
-import loadSampleData from "./database/loadDatabase.ts";
 import setUpScheduledTasks from "./utility/scheduledTasksSetup.ts";
 import {handleSlotUpdates} from "./services/reservationServices.ts";
->>>>>>> origin/Milestone4
+
 
 const port: number = 3000;
 const shouldLoadSampleData = process.env.LOAD_SAMPLE_DATA === "true" || process.env.LOAD_SAMPLE_DATA === "1";
