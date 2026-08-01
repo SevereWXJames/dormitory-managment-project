1776d196c2e1c65aecad57edbe92feb79837db84
janet-s-1 <jsong301@students.cs.ubc.ca>
2026-08-01 00:23:22 -0700

    Set CSP Headers and X-Content-Type-Options in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,34 @@ import { defineConfig } from 'vite'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
+import { cspProxyPlugin } from 'vite-plugin-content-security-policy';
+
+export const ENVIRONMENTS = ['production', 'staging', 'development'];
+export type Environment = typeof ENVIRONMENTS[number];
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        cspProxyPlugin<Environment>({
+            rules: {
+                "script-src": "'self'",
+                "default-src": "'self'",
+                "img-src": "'self'",
+                "frame-ancestors": "'none'",
+                "form-action": "'self'"
+            },
+            reportType: 'strict',
+        }),
+        react(),
+        tailwindcss()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    server:{
+        headers:{
+            "X-Content-Type-Options": "nosniff",
+        }
+    }
 })
